5a873464d98202f791f641b15bc52fdf8c3a69bd
Atlas-Unory <surfitasnierks@gmail.com>
2025-03-19 19:51:25 -0300

    Apredendo sobre o tipo tuple

---
+++ secao3/tipoTuple.py
@@ -0,0 +1,9 @@
+"""
+Tipo tupla - uma lista imutavel
+"""
+_ ,nome2, *_ = ['maria', 'helena', 'joao']
+print(nome2)
+
+#isso em baixo é uma tupla
+nomes = 'maria', 'fernanda', 'mateus'
+print(nomes)
