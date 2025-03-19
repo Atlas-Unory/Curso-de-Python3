e184281cf1734f2b6a3aab32bf5d9c2ac07d7388
Atlas-Unory <surfitasnierks@gmail.com>
2025-03-19 09:54:39 -0300

    Aprendendo a aterar o valor de listas

---
+++ secao3/autelrandoList.py
@@ -0,0 +1,17 @@
+lista = [10, 20, 30, 40]
+# Metodos úteis
+# pop() insert() append() del() clear() extend()
+numero = lista
+
+numero[1] = 400
+
+print(numero)
+
+#parte de concatenação de extenção de listas
+
+listaa = [1, 2, 3]
+listab = [4, 5, 6]
+
+listac = listaa + listab
+
+print(listac)
