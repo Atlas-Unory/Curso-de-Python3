a44b408ac50aedfea390d3e9b61de423a2e12fcc
Atlas-Unory <surfitasnierks@gmail.com>
2025-03-23 10:31:49 -0300

    Vendo alguns comandos do interpretador do python

---
+++ secao3/detalhesdointerpretadorpython.py
@@ -0,0 +1,7 @@
+# Enterpretador python
+# python mod.py = executa o mod
+# python -u = unbufferd
+# python -m mod = lib mod como script
+# python -c 'cmd' = comando
+# python -i mod.py = iterativo com o python
+# python 
