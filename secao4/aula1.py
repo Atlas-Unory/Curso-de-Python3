27912f2aec8007bebc74a8a91c0569dc9e660110
Atlas-Unory <surfitasnierks@gmail.com>
2025-03-26 16:56:19 -0300

    Primeira aula na secao4, aula de introdução a funções'

---
+++ secao4/aula1.py
@@ -0,0 +1,11 @@
+# Primeira aula sobre funções no python
+
+def imprimir(a):
+    print(a)
+
+def saudacao(nome):
+    print(f'Olá, {nome}')
+
+saudacao('maria')
+saudacao('vanessa')
+saudacao('emily')
