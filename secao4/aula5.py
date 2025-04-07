846675ff60142e9a39a2cf8040815e8928a2d758
Atlas-Unory <surfitasnierks@gmail.com>
2025-04-07 18:14:19 -0300

    Aula sobre higher order functions

---
+++ secao4/aula5.py
@@ -0,0 +1,13 @@
+# Heigher Order functions
+# funções de primeira classe
+
+def saudacao(msg, nome):
+    return f'{msg}, {nome}'
+
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+
+v = executa(saudacao, "bom dia", "luiz")
+print(v)
