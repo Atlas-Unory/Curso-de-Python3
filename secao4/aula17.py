83b2168640692e2019c1fa77cbedf0953568e237
Atlas Unory <surfitasnierks@gmail.com>
2025-07-11 12:35:18 -0300

    Mais detalhes sobre Iterables e Iterators (Iteráveis e Iteradores)

---
+++ secao4/aula17.py
@@ -0,0 +1,8 @@
+# Mais detalhes sobre Iterables e Iterators (Iteráveis e Iteradores)
+iterable = ['eu', 'tenho', '__iter__']
+iterator = iter(iterable)
+
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
