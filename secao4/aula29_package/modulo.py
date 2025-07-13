3b5c1d15fc9175211ddfef8dacd04901a9d43b28
Atlas Unory <surfitasnierks@gmail.com>
2025-07-13 09:07:28 -0300

    Importanto o modulo_b.py e chamando a função fala_oi

--- secao4/aula29_package/modulo.py
+++ secao4/aula29_package/modulo.py
@@ -4,6 +4,8 @@
 #     'nova_variavel',
 # ]
 
+from aula29_package.modulo_b import fala_oi
+
 variavel = "Alguma coisa"
 
 
@@ -12,3 +14,5 @@ def soma_do_modulo(x, y):
 
 
 nova_variavel = "OK"
+
+fala_oi()
