22350fe074177bd5739e4d12a6e4f8a620a94c33
Atlas Unory <surfitasnierks@gmail.com>
2026-04-08 16:09:54 -0300

    class - Classes são moldes para criar novos objetos

---
+++ secao5/aula200.py
@@ -0,0 +1,18 @@
+# class - Classes são moldes para criar novos objetos
+
+
+class Pessoa: ...
+
+
+p1 = Pessoa()
+p1.nome = "Luiz"
+p1.sobrenome = "Joana"
+print(p1.nome)
+print(p1.sobrenome)
+
+p2 = Pessoa()
+p2.nome = "Maria"
+p2.sobrenome = "Fernanda"
+
+print(p2.nome)
+print(p2.sobrenome)
