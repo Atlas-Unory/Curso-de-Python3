c9fbbf8fb431d5c4ce4f90c4fe5270aea855a72a
Atlas Unory <surfitasnierks@gmail.com>
2026-04-09 10:01:25 -0300

    Atributos de classe

---
+++ secao5/aula206.py
@@ -0,0 +1,23 @@
+# Atributos de classe
+
+
+class Pessoa:
+    ano_atual = 2022
+
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.ano_atual = 100
+
+    def get_ano_de_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+
+
+p1 = Pessoa("João", 34)
+p2 = Pessoa("Helena", 23)
+
+print(Pessoa.ano_atual)
+Pessoa.ano_atual = 1
+
+print(p1.get_ano_de_nascimento())
+print(p2.get_ano_de_nascimento())
